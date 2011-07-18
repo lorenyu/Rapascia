f6062f76b79576243a807ed7fdc1a1efa60ae579
kgarapaty <kgarapaty@gmail.com>
2011-07-18 06:27:04 +0000

    join and leave func

--- game/models/Player.js
+++ game/models/Player.js
@@ -2,4 +2,30 @@ var Player = module.exports = function() {
     this.id = Math.random();
     this.name = 'Player'+this.id;
     this.game = null;
+    
+    this.joinGame = function(game) {
+        this.game = game;
+        this.emit('message', {
+            type: 'joinGame',
+            player: this,
+            game: this.game
+        });
+        return true;
+    };
+        
+    this.leaveGame = function() {
+        if (!this.game) {
+            return false;
+        }
+        //TODO: function needs to be created
+        this.game.removePlayer(this);
+        this.emit('message', {
+            type: 'leaveGame',
+            player: this,
+            game: this.game
+        });
+            
+        this.game = null;
+        return true;
+    };
 };
