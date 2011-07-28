329821e7f09c0e9d23d090c6864723c62d5c9b39
lorenyu <yuloren@gmail.com>
2011-07-28 06:54:32 +0000

    Added tick method to GameClient

--- public/js/rapascia/GameClient.js
+++ public/js/rapascia/GameClient.js
@@ -4,10 +4,24 @@ var Rapascia = window.Rapascia = {};
 
 var GameClient = Rapascia.GameClient = function(socket) {
     this.socket = socket;
-    socket.on('tick', function(data) {
-        $('.time').text(data.time);
-    });
+    socket.on('tick', $.proxy(this.tick, this));
 };
+GameClient.prototype.tick = function(data) {
+    $('.time').text(data.time); // for debugging
+    
+    var commands = data.commands,
+        time = data.time,
+        timeElapsed;
+    
+    timeElapsed = time - this.time;    
+    
+    // process player commands
+    
+    // update game state
+    
+    this.time = time;
+};
+
 GameClient.prototype.sendCommand = function(command) {
     this.socket.emit('player-command', command);
 };
