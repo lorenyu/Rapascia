faf297f2b27813501698cfb48d93e73f3c16b64c
lorenyu <yuloren@gmail.com>
2011-07-29 07:38:13 +0000

    Added now method to gametime

--- game/GameTime.js
+++ game/GameTime.js
@@ -1,7 +1,10 @@
 var GameTime = module.exports = function() {
     this.update();
 };
-
 GameTime.prototype.update = function() {
-    this.millis = new Date().getTime();
+    this.millis = this.now();
 };
+GameTime.prototype.now = function() {
+    return new Date().getTime();
+};
+
