d5739c13597ad407db9969cf9674e0dce05c30bd
lorenyu <yuloren@gmail.com>
2011-08-03 09:41:11 +0000

    Player can set a tile to produce or defend

--- public/js/rapascia/GameClient.js
+++ public/js/rapascia/GameClient.js
@@ -81,11 +81,16 @@ Rapascia.models = {};
 var Game = Rapascia.models.Game = function() {
     this._players = [];
     this._map = new Map();
+    this._time = 0;
     
     // UI properties
     this._selectedTile = null;
     this._selectingUnits = null;
     this._selectedUnits = null;
+};
+Game.prototype.tick = function(time) {
+    this._time = time;
+    this.map().tick(time);
 };
 Game.prototype.players = function() {
     return this._players;
@@ -102,13 +107,13 @@ Game.prototype.addPlayer = function(player) {
             startingPosition = this.map().tiles()[0][0];
             break;
         case 2:
-            startingPosition = this.map().tiles()[11][11];
+            startingPosition = this.map().tiles()[8][8];
             break;
         case 3:
-            startingPosition = this.map().tiles()[0][11];
+            startingPosition = this.map().tiles()[0][8];
             break;
         case 4:
-            startingPosition = this.map().tiles()[11][0];
+            startingPosition = this.map().tiles()[8][0];
             break;
     }
     
@@ -165,10 +170,12 @@ Game.prototype.selectedUnits = function(units) {
  * Map Model
  */
 var Map = Rapascia.models.Map = function() {
+    
+    this._time = 0;
 
     // hardcode map for now
-    var numRows = this.numRows = 12,
-        numCols = this.numCols = 12,
+    var numRows = this.numRows = 9,
+        numCols = this.numCols = 9,
         tiles = this._tiles = [], tile,
         i,
         j;
@@ -182,6 +189,12 @@ var Map = Rapascia.models.Map = function() {
             tiles[i].push(tile);
         }
     }
+};
+Map.prototype.tick = function(time) {
+    this._time = time;
+    _.each(this.tiles(), function(row) {
+        _.invoke(row, 'tick', time);
+    });
 };
 Map.prototype.tiles = function() {
     return this._tiles;
@@ -198,10 +211,14 @@ Map.prototype.isAdjacent = function(tileA, tileB) {
  * Tile Model
  */
 var Tile = Rapascia.models.Tile = function(map, id) {
+    this._time = 0;
     this._map = map;
     this._id = id;
     this._units = [];
+    this._mode = 'stopped';
     Tile._tilesById[id] = this;
+    
+    this._productionAmount = 0;
     
     // UI properties
     this._selected = false;
@@ -209,6 +226,27 @@ var Tile = Rapascia.models.Tile = function(map, id) {
 Tile._tilesById = {};
 Tile.get = function(id) {
     return Tile._tilesById[id];
+};
+Tile.prototype.tick = function(time) {
+    var timeElapsed = time - this._time,
+        numUnitsProduced,
+        player = this.player();
+    if (this.mode() == 'producing') {
+        this._productionAmount += (timeElapsed / 1000) * this.units().length * Unit.PRODUCTION_PER_UNIT;
+        
+        numUnitsProduced = Math.floor(this._productionAmount / Unit.COST_PER_UNIT);
+        this._productionAmount = this._productionAmount % Unit.COST_PER_UNIT;
+        
+        this.addUnits(_.map(_.range(numUnitsProduced), function() {
+            var unit = new Unit(player);
+            unit.mode('producing');
+            return unit;
+        }));
+        
+    }
+    this._time = time;
+    
+    _.invoke(this.units(), 'tick', time);
 };
 Tile.prototype.map = function() {
     return this._map;
@@ -233,6 +271,13 @@ Tile.prototype.addUnits = function(units) {
 };
 Tile.prototype.removeUnits = function(units) {
     this.units(_.difference(this.units(), units));
+};
+Tile.prototype.mode = function(mode) {
+    if (mode === undefined) {
+        return this._mode;
+    }
+    this._mode = mode;
+    _.invoke(this.units(), 'mode', mode);
 };
 Tile.prototype.player = function() {
     if (this.units().length > 0) {
@@ -262,6 +307,7 @@ Tile.prototype.toggleSelected = function() {
 var Unit = Rapascia.models.Unit = function(player) {
     this._id = Unit._nextId;
     Unit._nextId += 1;
+    this._time = 0;
     this._player = player;
     this._mode = 'stopped';
     this._health = Unit.HEALTH_PER_UNIT;
@@ -277,8 +323,13 @@ var Unit = Rapascia.models.Unit = function(player) {
 Unit._nextId = 1;
 Unit._unitsById = {};
 Unit.HEALTH_PER_UNIT = 10;
+Unit.PRODUCTION_PER_UNIT = 4;
+Unit.COST_PER_UNIT = 100;
 Unit.get = function(id) {
     return Unit._unitsById[id];
+};
+Unit.prototype.tick = function(time) {
+    this._time = time;
 };
 Unit.prototype.id = function() {
     return this._id;
@@ -286,19 +337,12 @@ Unit.prototype.id = function() {
 Unit.prototype.player = function() {
     return this._player;
 };
-Unit.prototype.mode = function() {
-    if (arguments.length === 0) {
-        return this._mode;
-    }
-    this._mode = arguments[0];
-    return this;
-};
 Unit.prototype.damage = function() {
     var defaultDamage = 4;
     
     switch (this.mode()) {
-    case 'defending': return 1.25 * defaultDamage;
-    case 'producing': return 0.50 * defaultDamage;
+    case 'defending': return 1.30 * defaultDamage;
+    case 'producing': return 0.35 * defaultDamage;
     }
     return defaultDamage;
 };
@@ -313,6 +357,12 @@ Unit.prototype.tile = function(tile) {
         return this._tile;
     }
     this._tile = tile;
+};
+Unit.prototype.mode = function(mode) {
+    if (mode === undefined) {
+        return this._mode;
+    }
+    this._mode = mode;
 };
 Unit.prototype.isTransitioning = function() {
     return this._cooldown > 0;
@@ -453,6 +503,23 @@ var GameClient = Rapascia.GameClient = function(socket) {
             return false;
         }
     });
+    
+    $('.tile .actions .produce').live({
+        'mousedown': function(event) {
+            var $this = $(this),
+                tile = $this.parents('.tile').rapascia('getModel')[0];
+            gameClient.sendCommand(new Rapascia.commands.Produce(tile));
+        }
+    });
+    
+    $('.tile .actions .defend').live({
+        'mousedown': function(event) {
+            var $this = $(this),
+                tile = $this.parents('.tile').rapascia('getModel')[0];
+            gameClient.sendCommand(new Rapascia.commands.Defend(tile));
+        }
+    });
+    
     /*
     $('.tile .move-all-units-btn').live('mouseover', function(event) {
         $this = $(this);
@@ -479,10 +546,10 @@ GameClient.prototype.tick = function(data) {
     _.each(commands, _.bind(this.execute, this));
     
     // update game state
-    $('.game').html(Rapascia.renderers.gameRenderer.call(this.game));
+    this.game.tick(time);
     
     // render
-    
+    $('.game').html(Rapascia.renderers.gameRenderer.call(this.game));
     
     this.time = time;
 };
@@ -492,6 +559,10 @@ GameClient.prototype.execute = function(command) {
     case 'start-game':
         break;
     case 'move': MoveCommand.deserialize(command).execute();
+        break;
+    case 'produce': ProduceCommand.deserialize(command).execute();
+        break;
+    case 'defend': DefendCommand.deserialize(command).execute();
         break;
     }
 };
@@ -504,6 +575,10 @@ GameClient.prototype.sendCommand = function(command) {
  ***********************/
  
 Rapascia.commands = {};
+
+/**
+ * MoveCommand
+ */
 var MoveCommand = Rapascia.commands.Move = function(units, from, to) {
     //console.log(units);
     this.name = 'move';
@@ -591,6 +666,54 @@ MoveCommand.deserialize = function(command) {
         to = Tile.get(command.to);
     
     return new MoveCommand(units, from, to);
+};
+
+/**
+ * ProduceCommand
+ */
+var ProduceCommand = Rapascia.commands.Produce = function(tile) {
+    this.name = 'produce';
+    this.tile = tile;
+};
+ProduceCommand.prototype.execute = function() {
+
+    this.tile.mode('producing');
+
+};
+ProduceCommand.prototype.serialize = function() {
+    return {
+        name: this.name,
+        tile: this.tile.id()
+    };
+};
+ProduceCommand.deserialize = function(command) {
+    var tile = Tile.get(command.tile);
+    
+    return new ProduceCommand(tile);
+};
+
+/**
+ * DefendCommand
+ */
+var DefendCommand = Rapascia.commands.Defend = function(tile) {
+    this.name = 'defend';
+    this.tile = tile;
+};
+DefendCommand.prototype.execute = function() {
+
+    this.tile.mode('defending');
+
+};
+DefendCommand.prototype.serialize = function() {
+    return {
+        name: this.name,
+        tile: this.tile.id()
+    };
+};
+DefendCommand.deserialize = function(command) {
+    var tile = Tile.get(command.tile);
+    
+    return new DefendCommand(tile);
 };
 
 })(window);
